c1c5b030990d06d67b103a37f2e97e0606fb23da
BenjaminHecker <benhecker7@gmail.com>
2023-04-15 00:30:48 -0400

    Added movement difficulty options Tweaked player animation for snappier snare hits

--- Assets/Scripts/SpotManager.cs
+++ Assets/Scripts/SpotManager.cs
@@ -11,6 +11,9 @@ public class SpotManager : MonoBehaviour, IOnCue, IOnBeat
     private bool firstSpot = true;
     private int dist = 2;
 
+    public enum Difficulty { Easy, Medium, Hard }
+    private Difficulty difficulty = Difficulty.Hard;
+
     private void Start()
     {
         RhythmManager.onCue += OnCue;
@@ -49,41 +52,51 @@ public class SpotManager : MonoBehaviour, IOnCue, IOnBeat
     {
         List<Vector2> outline = new List<Vector2>();
 
-        //for (int x = -dist; x <= dist; x++)
-        //{
-        //    Vector2 topOffset = new Vector2(x, dist);
-        //    Vector2 bottomOffset = new Vector2(x, -dist);
+        if (difficulty == Difficulty.Hard)
+        {
+            for (int x = -dist; x <= dist; x++)
+            {
+                Vector2 topOffset = new Vector2(x, dist);
+                Vector2 bottomOffset = new Vector2(x, -dist);
 
-        //    if (GridManager.InBounds(prevPos + topOffset)) outline.Add(topOffset);
-        //    if (GridManager.InBounds(prevPos + bottomOffset)) outline.Add(bottomOffset);
-        //}
+                if (GridManager.InBounds(prevPos + topOffset)) outline.Add(topOffset);
+                if (GridManager.InBounds(prevPos + bottomOffset)) outline.Add(bottomOffset);
+            }
 
-        //for (int y = -dist + 1; y <= dist - 1; y++)
-        //{
-        //    Vector2 leftOffset = new Vector2(-dist, y);
-        //    Vector2 rightOffset = new Vector2(dist, y);
+            for (int y = -dist + 1; y <= dist - 1; y++)
+            {
+                Vector2 leftOffset = new Vector2(-dist, y);
+                Vector2 rightOffset = new Vector2(dist, y);
 
-        //    if (GridManager.InBounds(prevPos + leftOffset)) outline.Add(leftOffset);
-        //    if (GridManager.InBounds(prevPos + rightOffset)) outline.Add(rightOffset);
-        //}
+                if (GridManager.InBounds(prevPos + leftOffset)) outline.Add(leftOffset);
+                if (GridManager.InBounds(prevPos + rightOffset)) outline.Add(rightOffset);
+            }
+        }
+        else
+        {
+            Vector2 topOffset = new Vector2(0, dist);
+            Vector2 bottomOffset = new Vector2(0, -dist);
+            Vector2 leftOffset = new Vector2(-dist, 0);
+            Vector2 rightOffset = new Vector2(dist, 0);
 
-        Vector2 topOffset = new Vector2(0, dist);
-        Vector2 bottomOffset = new Vector2(0, -dist);
-        Vector2 leftOffset = new Vector2(-dist, 0);
-        Vector2 rightOffset = new Vector2(dist, 0);
-        Vector2 topRightOffset = new Vector2(dist, dist);
-        Vector2 topLeftOffset = new Vector2(-dist, dist);
-        Vector2 bottomRightOffset = new Vector2(dist, -dist);
-        Vector2 bottomLeftOffset = new Vector2(-dist, -dist);
+            if (GridManager.InBounds(prevPos + topOffset)) outline.Add(topOffset);
+            if (GridManager.InBounds(prevPos + bottomOffset)) outline.Add(bottomOffset);
+            if (GridManager.InBounds(prevPos + leftOffset)) outline.Add(leftOffset);
+            if (GridManager.InBounds(prevPos + rightOffset)) outline.Add(rightOffset);
 
-        if (GridManager.InBounds(prevPos + topOffset)) outline.Add(topOffset);
-        if (GridManager.InBounds(prevPos + bottomOffset)) outline.Add(bottomOffset);
-        if (GridManager.InBounds(prevPos + leftOffset)) outline.Add(leftOffset);
-        if (GridManager.InBounds(prevPos + rightOffset)) outline.Add(rightOffset);
-        if (GridManager.InBounds(prevPos + topRightOffset)) outline.Add(topRightOffset);
-        if (GridManager.InBounds(prevPos + topLeftOffset)) outline.Add(topLeftOffset);
-        if (GridManager.InBounds(prevPos + bottomRightOffset)) outline.Add(bottomRightOffset);
-        if (GridManager.InBounds(prevPos + bottomLeftOffset)) outline.Add(bottomLeftOffset);
+            if (difficulty == Difficulty.Medium)
+            {
+                Vector2 topRightOffset = new Vector2(dist, dist);
+                Vector2 topLeftOffset = new Vector2(-dist, dist);
+                Vector2 bottomRightOffset = new Vector2(dist, -dist);
+                Vector2 bottomLeftOffset = new Vector2(-dist, -dist);
+
+                if (GridManager.InBounds(prevPos + topRightOffset)) outline.Add(topRightOffset);
+                if (GridManager.InBounds(prevPos + topLeftOffset)) outline.Add(topLeftOffset);
+                if (GridManager.InBounds(prevPos + bottomRightOffset)) outline.Add(bottomRightOffset);
+                if (GridManager.InBounds(prevPos + bottomLeftOffset)) outline.Add(bottomLeftOffset);
+            }
+        }
 
         return outline[Random.Range(0, outline.Count)];
     }
